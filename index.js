b971fd4f55de11fd6902a08f6d238867b56a2c7f
Babaralihyd <162362468+Babaralihyd@users.noreply.github.com>
2024-03-12 17:13:34 -1200

    types script book practice

---
+++ index.js
@@ -0,0 +1,34 @@
+// let hatprice =300;
+// console.log(`Hat price : ${hatprice}`) ;
+ 
+// let bootprice = '180'
+// console.log (`bootprice :  ${bootprice } `)
+
+
+// conditional stantements  (if ; else )        (logical operators )
+
+// if (bootprice ==100){
+//     console.log ('you are right ')
+//     console .log (20 -  15) }
+    
+//     else if (bootprice == 180  ){console.log ('bootprice==180')}
+
+//     else {console.log ('you are wrong ')}
+
+// symbol data types 
+// let greeting1 = 'hellow ';
+// let greeting2 = 'hellow ';
+// console.log(greeting1 == greeting2); 
+
+// let greeting3 = Symbol ('world ');
+// let greeting4 = 'hellow '; 
+// console.log(greeting3 == greeting4); 
+
+// undertanding types coerican 
+let pen = 200
+let book ="200 "
+console.log (  Number(pen) +Number( book) )
+console.log (pen == book )
+console.log (pen === book )
+
+
